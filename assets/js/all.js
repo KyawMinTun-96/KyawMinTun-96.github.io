444e41733a203c7d0c7da3d9b37602daa0108df2
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2022-09-07 22:58:13 +0630

    Update job exp date

--- assets/js/all.js
+++ assets/js/all.js
@@ -497,6 +497,7 @@ form.addEventListener('submit', e => {
 window.addEventListener('load', function() {
 
     setTimeout(removeLoader, 2000);
+    dateCalculate();
     
 });
 
@@ -510,3 +511,81 @@ function removeLoader() {
 
     });
 }
+
+
+function dateCalculate() {
+
+    var sDate = new Date('2021-11-26');
+
+    //get the current date from the system
+    var now = new Date();
+    
+    //extract the year, month, and date from user date input
+    var sDateYear = sDate.getYear();
+    var sDateMonth = sDate.getMonth();
+    var sDateDate = sDate.getDate();
+    
+    //extract the year, month, and date from current date
+    var currentYear = now.getYear();
+    var currentMonth = now.getMonth();
+    var currentDate = now.getDate();
+	
+    //declare a variable to collect the year, month, and days
+    var exp = {};
+    var expString = "";
+  
+    //get years
+    yearExp = currentYear - sDateYear;
+	
+    //get months
+    if (currentMonth >= sDateMonth)
+      //get months when current month is greater
+      var monthExp = currentMonth - sDateMonth;
+    else {
+      yearExp--;
+      var monthExp = 12 + currentMonth - sDateMonth;
+    }
+
+    //get days
+    if (currentDate >= sDateDate)
+      //get days when the current date is greater
+      var dateExp = currentDate - sDateDate;
+    else {
+      monthExp--;
+      var dateExp = 31 + currentDate - sDateDate;
+
+      if (monthExp < 0) {
+        monthExp = 11;
+        yearExp--;
+      }
+    }
+    //group the age in a single variable
+    exp = {
+    years: yearExp,
+    months: monthExp,
+    days: dateExp
+    };
+      
+      
+    if ( (exp.years > 0) && (exp.months > 0) && (exp.days > 0) )
+       expString = exp.years + " years, " + exp.months + " months, and " + exp.days + " days";
+    else if ( (exp.years == 0) && (exp.months == 0) && (exp.days > 0) )
+       expString = "Only " + exp.days + " days";
+    //when current month and date is same as birth date and month
+    else if ( (exp.years > 0) && (exp.months == 0) && (exp.days == 0) )
+       expString = exp.years +  " years";
+    else if ( (exp.years > 0) && (exp.months > 0) && (exp.days == 0) )
+       expString = exp.years + " years and " + exp.months + " months";
+    else if ( (exp.years == 0) && (exp.months > 0) && (exp.days > 0) )
+       expString = exp.months + " months and " + exp.days + " days";
+    else if ( (exp.years > 0) && (exp.months == 0) && (exp.days > 0) )
+       expString = exp.years + " years, and" + exp.days + " days";
+    else if ( (exp.years == 0) && (exp.months > 0) && (exp.days == 0) )
+       expString = exp.months + " months";
+    //when current date is same as dob(date of birth)
+    else expString = "Something"; 
+
+    return document.getElementById("exp").innerHTML = expString; 
+             
+  }
+
