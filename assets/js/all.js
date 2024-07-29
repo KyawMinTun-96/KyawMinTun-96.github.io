b244907c92bfbb0754b1fb1bb8737939c31657d8
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2024-07-29 16:21:09 +0700

    modify javascript

--- assets/js/all.js
+++ assets/js/all.js
@@ -681,91 +681,91 @@ function removeLoader() {
 }
 
 
-function dateCalculate(startDate, endDate, ID) {
+// function dateCalculate(startDate, endDate, ID) {
 
-    var sDate = new Date(startDate);
+//     var sDate = new Date(startDate);
 
-    //get the current date from the system
-    var now = new Date(endDate);
+//     //get the current date from the system
+//     var now = new Date(endDate);
 
-    //extract the year, month, and date from user date input
-    var sDateYear = sDate.getYear();
-    var sDateMonth = sDate.getMonth();
-    var sDateDate = sDate.getDate();
+//     //extract the year, month, and date from user date input
+//     var sDateYear = sDate.getYear();
+//     var sDateMonth = sDate.getMonth();
+//     var sDateDate = sDate.getDate();
 
-    //extract the year, month, and date from current date
-    var currentYear = now.getYear();
-    var currentMonth = now.getMonth();
-    var currentDate = now.getDate();
+//     //extract the year, month, and date from current date
+//     var currentYear = now.getYear();
+//     var currentMonth = now.getMonth();
+//     var currentDate = now.getDate();
 
-    //declare a variable to collect the year, month, and days
-    var exp = {};
-    var expString = "";
+//     //declare a variable to collect the year, month, and days
+//     var exp = {};
+//     var expString = "";
 
-    //get years
-    yearExp = currentYear - sDateYear;
-
-
-
-    //get months
-    if (currentMonth >= sDateMonth)
-        //get months when current month is greater
-        var monthExp = currentMonth - sDateMonth;
-
-    else {
-        yearExp--;
-        var monthExp = 12 + currentMonth - sDateMonth;
-    }
-
-    //get days
-    if (currentDate >= sDateDate)
-        //get days when the current date is greater
-        var dateExp = currentDate - sDateDate;
-    else {
-        monthExp--;
-        var dateExp = 31 + currentDate - sDateDate;
-
-        if (monthExp < 0) {
-            monthExp = 11;
-            yearExp--;
-        }
-    }
-
-    //group the age in a single variable
-    exp = {
-        years: yearExp,
-        months: monthExp,
-        days: dateExp
-    };
-
-
-    if ((exp.years > 0) && (exp.months > 0) && (exp.days > 0))
-        expString = exp.years + " years, " + exp.months + " months, and " + exp.days + " days";
-    else if ((exp.years == 0) && (exp.months == 0) && (exp.days > 0))
-        expString = exp.days + " days";
-    //when current month and date is same as birth date and month
-    else if ((exp.years > 0) && (exp.months == 0) && (exp.days == 0))
-        expString = exp.years + " years";
-    else if ((exp.years > 0) && (exp.months > 0) && (exp.days == 0))
-        expString = exp.years + " years and " + exp.months + " months";
-    else if ((exp.years == 0) && (exp.months > 0) && (exp.days > 0))
-        expString = exp.months + " months and " + exp.days + " days";
-    else if ((exp.years > 0) && (exp.months == 0) && (exp.days > 0))
-        expString = exp.years + " years, and " + exp.days + " days";
-    else if ((exp.years == 0) && (exp.months > 0) && (exp.days == 0))
-        expString = exp.months + " months";
-    //when current date is same as dob(date of birth)
-    else expString = "Something";
-
-    return document.getElementById(ID).innerHTML = expString;
-
-}
+//     //get years
+//     yearExp = currentYear - sDateYear;
 
 
 
+//     //get months
+//     if (currentMonth >= sDateMonth)
+//         //get months when current month is greater
+//         var monthExp = currentMonth - sDateMonth;
 
-dateCalculate('2021-11-26', '2023-1-26', 'galaxy_exp');
-dateCalculate('2023-03-28', '2024-04-12', 'nex4_exp');
+//     else {
+//         yearExp--;
+//         var monthExp = 12 + currentMonth - sDateMonth;
+//     }
+
+//     //get days
+//     if (currentDate >= sDateDate)
+//         //get days when the current date is greater
+//         var dateExp = currentDate - sDateDate;
+//     else {
+//         monthExp--;
+//         var dateExp = 31 + currentDate - sDateDate;
+
+//         if (monthExp < 0) {
+//             monthExp = 11;
+//             yearExp--;
+//         }
+//     }
+
+//     //group the age in a single variable
+//     exp = {
+//         years: yearExp,
+//         months: monthExp,
+//         days: dateExp
+//     };
+
+
+//     if ((exp.years > 0) && (exp.months > 0) && (exp.days > 0))
+//         expString = exp.years + " years, " + exp.months + " months, and " + exp.days + " days";
+//     else if ((exp.years == 0) && (exp.months == 0) && (exp.days > 0))
+//         expString = exp.days + " days";
+//     //when current month and date is same as birth date and month
+//     else if ((exp.years > 0) && (exp.months == 0) && (exp.days == 0))
+//         expString = exp.years + " years";
+//     else if ((exp.years > 0) && (exp.months > 0) && (exp.days == 0))
+//         expString = exp.years + " years and " + exp.months + " months";
+//     else if ((exp.years == 0) && (exp.months > 0) && (exp.days > 0))
+//         expString = exp.months + " months and " + exp.days + " days";
+//     else if ((exp.years > 0) && (exp.months == 0) && (exp.days > 0))
+//         expString = exp.years + " years, and " + exp.days + " days";
+//     else if ((exp.years == 0) && (exp.months > 0) && (exp.days == 0))
+//         expString = exp.months + " months";
+//     //when current date is same as dob(date of birth)
+//     else expString = "Something";
+
+//     return document.getElementById(ID).innerHTML = expString;
+
+// }
+
+
+
+
+// dateCalculate('2021-11-26', '2023-1-26', 'galaxy_exp');
+// dateCalculate('2023-03-28', '2024-04-12', 'nex4_exp');
 
 
 
@@ -781,7 +781,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to send data to your server
     function sendVisitorInfo(data) {
         // Replace the following URL with the endpoint on your server to handle the data
-        const endpoint = 'https://prod-50.southeastasia.logic.azure.com:443/workflows/efc3a04d12a34a52b9b8c8720c5756db/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=-Y6B4QDJoLFJ3zto74rofhqk2emUnaBpnZl-cryJeVE';
+        // const endpoint = 'https://prod-50.southeastasia.logic.azure.com:443/workflows/efc3a04d12a34a52b9b8c8720c5756db/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=-Y6B4QDJoLFJ3zto74rofhqk2emUnaBpnZl-cryJeVE';
+
+        const endpoint = 'https://prod2-59.southeastasia.logic.azure.com:443/workflows/42ef4487b5ae41a29170530ddb701d60/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IQyAHtZqWqNMXhjLTsiOps8HHwhzOv3iwWhgOAoaGV0';
         
         // Use fetch API to send data to the server
         fetch(endpoint, {
@@ -838,7 +840,6 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         timestamp: new Date().toISOString(),
     };
-
 
     // Function to get plugin information
     function getPluginInfo() {
