147e86d39bc2fc03152a681dfd548ec89e7ce838
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2024-08-12 17:54:34 +0700

    add error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,17 +1,15 @@
-import {Link} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Error() {
-    return (
+    const navigate = useNavigate();
 
-        <div className='error'>
-            <p>Oops!</p>
-            <h1>404</h1>
-            <p>We can't find the page you're looking for.....</p>
-            <Link className='error-btn' to='/'>Go back home</Link>
-        </div>
-
-    );
+    useEffect(() => {
+      // Redirect to 404.html
+      window.location.href = '/404.html';
+    }, [navigate]);
+  
+    return null; // No UI, as the redirect happens immediately
 }
-
 
 export default Error;
